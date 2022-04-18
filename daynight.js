d1eab0c4f0e47ad2489c2b690afebc0210494d29
RooT_Kim <bdd3452@gmail.com>
2022-04-18 11:57:32 +0900

    Use Jquery and Import & Export

---
+++ daynight.js
@@ -0,0 +1,26 @@
+function night() {
+  document.querySelector("body").style.backgroundColor = "black";
+  document.querySelector("body").style.color = "white";
+  let as = document.querySelectorAll("a");
+  for (let i = 0; i < as.length; i = i + 1) {
+    as[i].style.color = "white";
+  }
+}
+export function day() {
+  // document.querySelector("body").style.backgroundColor = "white";
+  $("body").css("background-color", "white");
+  // document.querySelector("body").style.color = "black";
+  $("body").css("color", "black");
+  // let as = document.querySelectorAll("a");
+  // for (let i = 0; i < as.length; i = i + 1) {
+  //   as[i].style.color = "black";
+  // }
+  $("a").css("color", "black");
+}
+export function dayNight(mode) {
+  if (mode === "night") {
+    night();
+  } else {
+    day();
+  }
+}
